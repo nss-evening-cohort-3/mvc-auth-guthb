b30c340a0a730dd72f885e0369cf10d36681ef62
Bradley Guthrie <guppy96@gmail.com>
2016-11-07 10:05:11 -0600

    corrected issue with blogcontext not having correct mode, successfuly enabled, added migration and updated database

---
+++ MvcAuth/MvcAuth/DAL/BlogContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using MvcAuth.Models;
+
+namespace MvcAuth.DAL
+{
+    public class BlogContext : ApplicationDbContext
+    {
+        public virtual DbSet<BlogPost> Posts { get; set; }
+    }
+}
